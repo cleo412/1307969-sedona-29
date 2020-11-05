c927bfac534a036a0f9dd2aec4fa4a512d1e02b6
cleo412 <semenenko412@gmail.com>
2020-11-05 15:04:41 +0300

    ЗАЩИТА ПРОЕКТА СЕДОНА СЕМЕНЕНКО АННА, вторая проверка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
 const modal = document.querySelector(".search__modal");
-const button = document.querySelector(".search__button-description");
+const form = modal.querySelector(".search__form");
+const toggleModalButton = document.querySelector(".search__button-modal");
+const submitButton = modal.querySelector(".modal__button");
+const firstInput = form.querySelector("input");
+const arrival = form.querySelector(".input-big-arrival");
+const departure = form.querySelector(".input-big-departure");
+const adults = form.querySelector(".search__total-adults");
+const children = form.querySelector(".search__total-children");
 
-button.onclick = function () {
-  modal.classList.toggle("modal-open");
-};
+toggleModalButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  firstInput.focus();
+  if (modal) {
+    modal.classList.toggle("search__modal-close");
+  }
+});
+
+submitButton.addEventListener("click", function (evt) {
+  if (!form.checkValidity()) {
+    evt.preventDefault();
+    form.classList.add("search__modal-error");
+  } else {
+    localStorage.setItem("arrival", arrival.value);
+    localStorage.setItem("departure", departure.value);
+    localStorage.setItem("adults", adults.value);
+    localStorage.setItem("children", children.value);
+  }
+});
+
+form.addEventListener("animationend", function () {
+  form.classList.remove("search__modal-error");
+});
+
